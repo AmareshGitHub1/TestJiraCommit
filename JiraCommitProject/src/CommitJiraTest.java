a9b25238194d19288c448a6d4eed1b32dfc49a4a
amaresh-huchanur <amaresh.huchaur@ikea.com>
2019-03-07 23:57:54 +0100

    SAM-1 #comment corrected indent issue

--- JiraCommitProject/src/CommitJiraTest.java
+++ JiraCommitProject/src/CommitJiraTest.java
@@ -2,7 +2,7 @@
 public class CommitJiraTest {
 	
 	public static void main(String[] args) {
-		System.out.println("Commit Test Successfull");
+		System.out.println("Commit Test Successful");
 	}
 
 }
