be456496b23c965b1c9e4bec4d566fc36b8dd07e
amaresh-huchanur <amaresh.huchaur@ikea.com>
2019-03-08 00:01:15 +0100

    SAM-2 #time 1w 2d 4h 30m Total work logged

--- JiraCommitProject/src/TestJiraCommit.java
+++ JiraCommitProject/src/TestJiraCommit.java
@@ -2,7 +2,7 @@ public class TestJiraCommit {
 	
 public static void main(String[] args) {
 	
-	System.out.println("Commit with Jira ID");
+	System.out.println("Commit with Jira ID Successfull");
 	
  }
 
