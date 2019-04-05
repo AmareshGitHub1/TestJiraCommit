de3cf9f0f681cee4f19484ad504366e07e5069fa
amaresh-huchanur <amaresh.sh@gmail.com>
2019-04-05 08:33:53 +0200

    SAM-1#comment Deadlock issue fixed, lock time out still appears

--- JiraCommitProject/src/TestJiraCommit.java
+++ JiraCommitProject/src/TestJiraCommit.java
@@ -2,7 +2,7 @@ public class TestJiraCommit {
 	
 public static void main(String[] args) {
 	
-	System.out.println("Commit with Jira ID Successfully Done3");
+	System.out.println("Commit with Jira ID Successfully Done4");
 	
  }
 
